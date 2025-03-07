41c1fb486e74ecbb1b7c075bd21331c3a104324d
Jessa773 <jdatul773@gmail.com>
2025-03-06 22:50:19 -0800

    Main.php

--- Main.php
+++ Main.php
@@ -1,9 +1,12 @@
 <?php
 
 $employees = [
-    new FullTimeEmployee("Jessa Dy", 101, "Manager", 10000),
-   
-
+    new FullTimeEmployee("Jessa Dy", 101, "Manager", 30000),
+    new PartTimeEmployee("Jane Board", 102, "Assistant", 400, 80),
+    new FullTimeEmployee("Luisy Salve", 103, "Secretary", 40000),
+    new PartTimeEmployee("Jom Ngars", 104, "Designer", 400, 60),
+    new FullTimeEmployee("Charles Braid", 105, "Finance", 60000),
+    
 ];
 
 foreach ($employees as $employee) {
